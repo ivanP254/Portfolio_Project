b5f7db9c965cbeaae678c1430878f8fc71c08a3d
Ivan <ivan2022@list.ru>
2024-02-29 00:59:04 +0300

    first commit

---
+++ vite-project/src/data.js
@@ -0,0 +1,102 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://1.downloader.disk.yandex.ru/preview/9c2d76227aa1cba749b6c303a4ba654b6de57f21396d8e321c2a5bad1d0429bb/inf/-6R1BY0Eeo-U-UNgY0YnOIoLpZvwHygf3bz1mJhe0ke_mK5zdCuwYvawFjUask9URas_jxiZ4DaGr33Pv_7CPA%3D%3D?uid=408680565&filename=%D0%A1%D0%B5%D1%80%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%82%201.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=408680565&tknv=v2&size=3840x2097",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://3.downloader.disk.yandex.ru/preview/0717638516f302f93593374928e8951a1ca0e64400dcaaa94f745ef779fcb3f2/inf/bgJFqXBC0OxmWqwC1GTzkdBMqeHWYdBNED3PBwW9wHNpIn_6bpnkR_R-NvT9lE8PLe4mxgdQmAziOX9hrtRyIw%3D%3D?uid=408680565&filename=%D0%A0%D0%B8%D1%81%D1%83%D0%BD%D0%BE%D0%BA%202.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=408680565&tknv=v2&size=3840x2097",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+{
+  name: "JavaScript",
+  progress: "70",
+  state: "Middle",  
+
+},
+{ 
+  name: "Next.js",
+  progress: "24",
+  state: "Junior",
+},
+{
+  name: "React",
+  progress: "89",
+  state: "Junior",
+},
+
+{
+  name: "Node",
+  progress: "14",
+  state: "Middle",
+},
+{ 
+  name: "Linux",
+  progress: "64",
+  state: "Middle",
+},
+{
+  name: "Material UI",
+  progress: "100",
+  state: "Middle",
+},
+];
